56ba316ee257372994fd0be4c0364ac33ee59d0b
subramanian-s-043 <113493600+subramanian-s-043@users.noreply.github.com>
2023-06-23 12:25:08 +0530

    update fetching type

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -34,7 +34,7 @@ const getTaskById=async(req,res)=>{
     }
     try{
         await Task.findOne({_id:req.params.id}).then((data)=>{
-                res.status(200).json([{data}])
+                res.status(200).json([data])
         }).catch((err)=>res.status(404).json({err}))
     }
     catch(error){
