4ff41f49c5e5bd12e23db3c45f770f139e002e45
Sridhar S <sridharsrinivasan9422@gmail.com>
2023-06-28 12:02:11 +0530

    .

--- models/Task.js
+++ models/Task.js
@@ -59,11 +59,11 @@ const taskSchema = new mongoose.Schema(
     targetrelease: {
       type: String
     },
-   type: {
-    type:String,
-    required:true
+  //  type: {
+  //   type:String,
+  //   required:true
     
-    }
+  //   }
   //  pdf: {
   //     data: Buffer,
   //     contentType: String,
